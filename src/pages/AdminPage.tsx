6ceb166fbe762b9bd63e7bad634a1a7da60ceece
ilia makarov <ilia.makarov@me.com>
2022-12-02 00:58:48 +0200

    Styled copy button like on github

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,21 +1,38 @@
 import { FC, useState, useMemo } from "react";
+import styled from "styled-components";
 import { v4 as uuidv4 } from "uuid";
 import { CopyToClipboard } from "react-copy-to-clipboard";
-import { faCopy } from "@fortawesome/free-solid-svg-icons";
+import { faCopy, faCheck } from "@fortawesome/free-solid-svg-icons";
 import {
-    H2,
     P1,
-    Stack,
-    Button,
+    H2,
+    Input,
+    IconButton,
     LoadingSpinner,
     useDeskproAppTheme,
     useInitialisedDeskproAppClient,
 } from "@deskpro/app-sdk";
 
+const Label = styled(H2)`
+    margin-bottom: 5px;
+`;
+
+const Description = styled(P1)`
+    margin-top: 8px;
+    margin-bottom: 16px;
+    color: ${({ theme }) => theme.colors.grey80};
+`;
+
 const AdminPage: FC = () => {
     const { theme } = useDeskproAppTheme();
     const [callbackUrl, setCallbackUrl] = useState<string|null>(null);
+    const [isCopy, setIsCopy] = useState<boolean>(false);
     const key = useMemo(() => uuidv4(), []);
+
+    const onClickCopy = () => {
+        setIsCopy(true);
+        setTimeout(() => setIsCopy(false), 2000);
+    };
 
     useInitialisedDeskproAppClient((client) => {
         client.oauth2()
@@ -29,22 +46,23 @@ const AdminPage: FC = () => {
 
     return (
         <>
-            <H2 style={{ marginBottom: "5px" }}>Callback URL</H2>
-            <Stack
-                style={{ borderColor: theme.colors.brandShade40, color: theme.colors.grey100 }}
-                justify="space-between"
-                align="center"
-            >
-                <div style={{ overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis", paddingRight: "15px" }}>
-                    {callbackUrl}
-                </div>
-                <CopyToClipboard text={callbackUrl}>
-                    <Button text="Copy" icon={faCopy} intent="secondary"/>
-                </CopyToClipboard>
-            </Stack>
-            <P1 style={{ marginBottom: "16px", marginTop: "8px", color: theme.colors.grey80 }}>
-                The callback URL will be required during Salesforce app setup
-            </P1>
+            <Label>Callback URL</Label>
+            <Input
+                disabled={true}
+                value={callbackUrl}
+                rightIcon={(
+                    <CopyToClipboard text={callbackUrl}>
+                        <IconButton
+                            minimal
+                            onClick={onClickCopy}
+                            icon={isCopy ? faCheck : faCopy}
+                            title="Copy"
+                            {...(isCopy ? { style: { color: theme.colors.green100 }} : {})}
+                        />
+                    </CopyToClipboard>
+                )}
+            />
+            <Description>The callback URL will be required during GitLab app setup</Description>
         </>
     )
 };
