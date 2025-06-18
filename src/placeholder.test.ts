a44e315647782b9ab40bb8a2bb69b4806339ad9f
OJ Abba <phavorabba@gmail.com>
2025-06-18 11:40:37 +0100

    [Chore] Add placeholder test

---
+++ src/placeholder.test.ts
@@ -0,0 +1,7 @@
+// This is here to prevent builds failing as the project doesn't have tests.
+// Remove this once tests have been added.
+describe("Placeholder test", () => {
+  test("placeholder", () => {
+    expect(true).toBe(true)
+  })
+})
